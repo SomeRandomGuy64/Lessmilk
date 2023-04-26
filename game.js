103879c25bed4776e7a41005af7c53f2d7d5776a
somerandomguy64 <faizan163123@gmail.com>
2023-04-26 11:09:47 +0100

    rendered stuff

--- game.js
+++ game.js
@@ -1,9 +1,58 @@
 class mainScene {
-  preload() {}
+  preload() {
+    this.load.image("player", "assets/player.png");
+    this.load.image("coin", "assets/coin.png");
+  }
 
-  create() {}
+  create() {
+    this.player = this.physics.add.sprite(100, 100, "player");
+    this.con = this.physics.add.sprite(300, 300, "coin");
 
-  update() {}
+    this.score = 0;
+    let style = {
+      font: "20px Arial",
+      fill: "#fff",
+    };
+
+    this.scoreText = this.add.text(20, 20, "score: " + this.score, style);
+
+    this.arrow = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.physics.overlap(this.player, this.coin)) {
+      this.hit();
+    }
+
+    if (this.arrow.right.isDown) {
+      this.player.x += 3;
+    } else if (this.arrow.left.isDown) {
+      this.player.x -= 3;
+    }
+
+    if (this.arrow.up.isDown) {
+      this.player.y -= 3;
+    } else if (this.arrow.down.isDown) {
+      this.player.y += 3;
+    }
+  }
+
+  hit() {
+    this.coin.x = Phaser.Math.Between(100, 600);
+    this.coin.y = Phaser.Math.Between(100, 300);
+
+    this.score += 10;
+
+    this.scoreText.setText("score: " + this.score);
+
+    this.tween.add({
+      targets: this.player,
+      duration: 200,
+      scaleX: 1.2,
+      scaleY: 1.2,
+      yoyo: true,
+    });
+  }
 }
 
 new Phaser.Game({
